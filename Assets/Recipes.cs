ea9dd91eb184f9e2dd34d3fec05b5232addd84fc
annaq1 <annaq1@uci.edu>
2018-07-06 12:34:26 -0700

    fud

---
+++ Assets/Recipes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipes : MonoBehaviour {
+
+    public int Chicken;
+    public int Salt;
+    public int Carrots;
+    public int Beef;
+    public int Lettuce;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
