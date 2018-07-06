b6aca3f4efa6f07159f73ced1bc861f31dc573d6
G0ldF1sh <kellyckc2017@gmail.com>
2018-07-06 14:57:58 -0700

    conveyer belt + borders complete

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections;
+﻿/** 
+ * Filename: Spawn.cs
+ * Description: Describes the spawner's behavior. Should spawn a random ingredient every second 
+ * Date: July 6, 2018 
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawn: MonoBehaviour {
-    public GameObject ingredient;
+    public GameObject borderObject;
     public bool stopSpawn = false;
-    public float spawnTime;
+    public float spawnIngredientTime;
+    public float spawnBorderTime;
     public float delay; 
     public float ySpeed;
 
@@ -14,19 +21,48 @@ public class Spawn: MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         //invoke repeating call (call method over and over) 
-        InvokeRepeating("spawnObject", spawnTime, delay);
-	}
+        InvokeRepeating("spawnIngredient", spawnIngredientTime, delay);
+        InvokeRepeating("spawnBorder", spawnBorderTime, delay);
+    }
 	
-	public void spawnObject()
+    /** 
+     * Function Name: spawnObject() 
+     * Function Prototype: public void spawnObject(); 
+     * Description: Spawns objects once every *delay* seconds, 
+     * Parameters: None 
+     * Side Effects: Creates ingredients in the game world that move in a downwards direction 
+     *              at ySpeed. 
+     * Error Conditions: None
+     * Return Value: None 
+     */
+	public void spawnIngredient()
     {
-        //pick random objct in ingredients[] 
+        //pick random object in ingredients[] and create object 
         int index = Random.Range(0, ingredients.Length);
         GameObject spawned = Instantiate(ingredients[index], transform.position, ingredients[index].transform.rotation);
         spawned.GetComponent<Rigidbody2D>().velocity = new Vector2(0, ySpeed);
 
-        if( stopSpawn )
+        /**
+        //spawn border 
+        GameObject border = Instantiate(borderObject, transform.position, transform.rotation);
+        border.GetComponent<Rigidbody2D>().velocity = new Vector2(0, ySpeed);
+    */
+        if ( stopSpawn )
         {
             CancelInvoke("spawnObject"); 
+        }
+
+    }
+
+    public void spawnBorder()
+    {
+        //spawn border 
+        GameObject border = Instantiate(borderObject, transform.position, transform.rotation);
+        border.GetComponent<Rigidbody2D>().velocity = new Vector2(0, ySpeed);
+
+        if (stopSpawn)
+        {
+            CancelInvoke("spawnObject");
         }
 
     }
