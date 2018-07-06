fafa990dd80b6048d797f125ffb8db07b9702ccc
annaq1 <annaq1@uci.edu>
2018-07-06 15:24:30 -0700

    ,

--- Assets/Scripts/RecipesText.cs
+++ Assets/Scripts/RecipesText.cs
@@ -6,13 +6,22 @@ using UnityEngine.UI;
 public class RecipesText : MonoBehaviour {
 
     public string[] NamesOfIngredients;
+    private int[] numberItems;
     public Text txt;
+    public int maxRecipe;
 
 	// Use this for initialization
 	void Start () {
         txt = GetComponent<Text>();
-        txt.text = "JIJO";
-	}
+        txt.text = "Recipe\n";
+        numberItems = new int[NamesOfIngredients.Length];
+        for (int i = 0; i < NamesOfIngredients.Length; ++i)
+        {
+            int rand = Random.Range(1, maxRecipe);
+            numberItems[i] = rand;
+            txt.text += NamesOfIngredients[i] + ": " + rand + "\n";
+        }
+ 	}
 	
 	// Update is called once per frame
 	void Update () {
