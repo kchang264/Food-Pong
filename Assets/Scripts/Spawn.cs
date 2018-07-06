448d99056dfba871cef7aa5fe605bb107690ab5c
G0ldF1sh <kellyckc2017@gmail.com>
2018-07-06 12:56:37 -0700

    Spawner spawns red boxes

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveIngredient : MonoBehaviour {
+public class Spawn: MonoBehaviour {
+    public GameObject ingredient;
+    public bool stopSpawn = false;
+    public float spawnTime;
+    public float delay; 
+    public float ySpeed; 
 
 	// Use this for initialization
 	void Start () {
-		
+        //invoke repeating call (call method over and over) 
+        InvokeRepeating("spawnObject", spawnTime, delay);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void spawnObject()
+    {
+        GameObject spawned = Instantiate(ingredient, transform.position, transform.rotation);
+        spawned.GetComponent<Rigidbody2D>().velocity = new Vector2(0, ySpeed);
+
+        if( stopSpawn )
+        {
+            CancelInvoke("spawnObject"); 
+        }
+
+    }
 }
