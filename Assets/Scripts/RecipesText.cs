b7d3cee075301ebb13471b40effaf70d6fef651d
annaq1 <annaq1@uci.edu>
2018-07-06 15:06:03 -0700

    ,

--- Assets/Scripts/RecipesText.cs
+++ Assets/Scripts/RecipesText.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Recipes : MonoBehaviour {
+public class RecipesText : MonoBehaviour {
 
     public string[] NamesOfIngredients;
+    public Text txt;
 
 	// Use this for initialization
 	void Start () {
+        txt = GetComponent<Text>();
+        txt.text = "JIJO";
 	}
 	
 	// Update is called once per frame
