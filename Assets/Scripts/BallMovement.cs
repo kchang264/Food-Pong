49d9cf6bdb86e5b1637a46a9020968746819cfc5
G0ldF1sh <kellyckc2017@gmail.com>
2018-07-06 19:37:43 -0700

    rips

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public Rigidbody2D rb; 
+public float force = 500f;
+
+public class BallMovement : MonoBehaviour {
+
+     
+
+    private void onMouseDown()
+    {
+        rb.velocity = newVector2(force, 0.0f);
+
+    }
+}
---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if( col.gameObject.name != "Pong" )
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
