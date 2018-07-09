91ce067434e786b18988e829302397d1e1e388a6
Kimberly Lo <32209256+kimberll@users.noreply.github.com>
2018-07-09 03:09:03 -0700

    Add belt and recipe display
    Add conveyor belt and recipe display to pong scene; need to fix to destroy ingredients

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -6,7 +6,9 @@ public class Destroy : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if( col.gameObject.name != "Pong" )
+        if( ! (col.gameObject.name == "Pong" 
+            || col.gameObject.name == "Ball" 
+            || col.gameObject.name == "Paddle"))
         {
             Destroy(col.gameObject);
         }
