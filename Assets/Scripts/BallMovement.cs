a1931948d4ced38ad5914fdce9b9de85100f9e6b
Kimberly Lo <32209256+kimberll@users.noreply.github.com>
2018-07-09 00:56:08 -0700

    Started pong
    Created basic pong scene with paddles, ball, and walls

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Vector2 vel;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("Paddle"))
+        {
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    
+    public float speed = 10.0f;
+    public float boundY = 6.0f;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        var vel = rb2d.velocity;
+        vel.y = speed;
+        rb2d.velocity = vel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var vel = rb2d.velocity;
+        var pos = transform.position;
+        if (pos.y > boundY)
+        {
+            pos.y = boundY;
+            rb2d.velocity = -vel;
+        }
+        else if (pos.y < -boundY)
+        {
+            pos.y = -boundY;
+            rb2d.velocity = -vel;
+        }
+        transform.position = pos;
+	}
+}
