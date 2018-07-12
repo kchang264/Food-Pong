fbfcc725821a5ea095d94cd6d3a5a09d5613bf99
G0ldF1sh <kellyckc2017@gmail.com>
2018-07-11 18:28:18 -0700

    Changed a BackButton Script to Change Button + cahnged a method name in the scirpt

---
+++ Assets/Scripts/SceneChangeButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* 
+ * Writes the behavior of a button that will change to another scene called "sceneName" 
+ */
+public class SceneChangeButton : MonoBehaviour {
+
+    public void changeScene( string sceneName )
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
