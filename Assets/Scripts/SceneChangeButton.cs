b92d078e2a341fb7f1a29f582205769d8438d2bb
G0ldF1sh <kellyckc2017@gmail.com>
2018-07-13 21:59:41 -0700

    THERE'S A WAY TO EXIT THIS HELL

--- Assets/Scripts/SceneChangeButton.cs
+++ Assets/Scripts/SceneChangeButton.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 /* 
  * Writes the behavior of a button that will change to another scene called "sceneName" 
+ * Also contains exit method 
  */
 public class SceneChangeButton : MonoBehaviour {
 
     public void changeScene( string sceneName )
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void exit()
+    {
+        Application.Quit(); 
     }
 }
