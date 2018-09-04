f5d2419d46fe60c54010e22d686a68cb9ff7b6e4
kchang264 <kellyckc2017@gmail.com>
2018-09-03 17:05:13 -0700

    Ball can't go through conveyer belt anymore

---
+++ Assets/Scripts/LeftBeltCols.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftBeltCols : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        //If ball enters trigger, then acts as a wall. 
+        if ( col.gameObject.tag == "Ball" )
+        {
+            Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
+            
+            //acting as a wall 
+            rb.velocity = new Vector2(-rb.velocity.x, -rb.velocity.y);
+        }
+    }
+}
